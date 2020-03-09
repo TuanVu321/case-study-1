42647ec084fbc5d09508fc71f88b5838ffbca868
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-03-09 17:36:38 +0700

    Add files via upload
    xong crud

--- app-book/js/my.js
+++ app-book/js/my.js
@@ -22,39 +22,45 @@ function Book(name, book_code, number, book_writing, type) {
         return this.type;
     }
 }
+
 //test
 let bookList = [];
 let book1 = new Book('doremon', '001', '100', 'Fujiko Fujio', 'Truyện tranh');
 bookList.push(book1);
+let book06 = new Book('doremon1', '0011', '1010', 'Fuj1iko Fujio', 'Truyệ1n tranh');
+bookList.push(book06);
 
 //hien thi
 function showList() {
     let html = '';
-    for (let i = 0; i < bookList.length; i++) {
+    for (let id = 0; id < bookList.length; id++) {
         html = html + '<tr>';
         html = html + '<td width="50px">';
-        html = html + i + 1;
+        html = html + (id + 1);
+        html = html + '</td>';
+        html = html + "<td id=\"name" + (id) + "\">";
+        html = html + bookList[id].getName();
+        html = html + '</td>';
+        html = html + "<td id=\"code" + (id) + "\">";
+        html = html + bookList[id].getBook_code();
+        html = html + '</td>';
+        html = html + "<td id=\"type" + (id) + "\">";
+        html = html + bookList[id].getType();
+        html = html + '</td>';
+        html = html + "<td id=\"number" + (id) + "\">";
+        html = html + bookList[id].getNumber();
+        html = html + '</td>';
+        html = html + "<td id=\"write" + (id) + "\">";
+        html = html + bookList[id].getBook_writing();
         html = html + '</td>';
         html = html + '<td>';
-        html = html + bookList[i].getName()+ "<button onclick='edit(this," + i +'getName'+")'>edit</button>";
+        html = html + "<button id =\"delete" + id + "\"  onclick=\"delete1(this.id.substring(this.id.length -1))\" >Delete</button>";
         html = html + '</td>';
         html = html + '<td>';
-        html = html + bookList[i].getBook_code();
+        html = html + "<button id =\"edit" + id + "\"  onclick=\"edit(this.id.substring(this.id.length -1))\">Edit</button>";
         html = html + '</td>';
         html = html + '<td>';
-        html = html + bookList[i].getType();
-        html = html + '</td>';
-        html = html + '<td>';
-        html = html + bookList[i].getNumber();
-        html = html + '</td>';
-        html = html + '<td>';
-        html = html + bookList[i].getBook_writing();
-        html = html + '</td>';
-        html = html + '<td>';
-        html = html + "<button onclick='delete1(this," + i + ")'>delete</button>";
-        html = html + '</td>';
-        html = html + '<td>';
-        html = html + "<button onclick='edit1(this," + i + ")'>edit</button>";
+        html = html + "<button id =\"save" + id + "\"  onclick=\"save(this.id.substring(this.id.length -1))\">Save</button>";
         html = html + '</td>';
         html = html + '</tr>';
     }
@@ -62,8 +68,10 @@ function showList() {
 }
 
 showList();
+
 //xoa
 function delete1(id) {
+    console.log(id);
     let r = confirm('Ban co muon xoa hay khong?');
     if (r == true) {
         bookList.splice(id, 1);
@@ -71,13 +79,83 @@ function delete1(id) {
     showList();
 }
 
-// chinh tung phan
-function edit(index,info) {
-    let e = prompt("ban muon doi thanh : ", bookList[index].info);
-    if (e !== null) {
-        bookList[index].info = e;
-    }
-    showList();
+// sua
+function edit(id) {
+    toggle(id)
 }
+
+function save(id) {
+    if (confirm("Bạn có muốn lưu?")) {
+        changeToggle(id);
+    } else {
+        toggle(id);
+    }
+
+}
+
+// hien thi bang chuyen doi
+
+
+function toggle(id) {
+    document.getElementById("name" + id).innerHTML = "<td><input id=\"name-toggle" + id + "\" value=\"" + bookList[id].name + "\"/></td>";
+    document.getElementById("code" + id).innerHTML = "<td><input id=\"code-toggle" + id + "\" value=\"" + bookList[id].book_code + "\"/></td>";
+    document.getElementById("type" + id).innerHTML = "<td><input id=\"type-toggle" + id + "\" value=\"" + bookList[id].type + "\"/></td>";
+    document.getElementById("number" + id).innerHTML = "<td><input id=\"number-toggle" + id + "\" value=\"" + bookList[id].number + "\"/></td>";
+    document.getElementById("write" + id).innerHTML = "<td><input id=\"write-toggle" + id + "\" value=\"" + bookList[id].book_writing + "\"/></td>";
+
+}
+
+//luu chuyen doi
+function changeToggle(id) {
+    let nameToggle = document.getElementById("name-toggle" + id),
+        book_codeToggle = document.getElementById("code-toggle" + id),
+        typeToggle = document.getElementById("type-toggle" + id),
+        numberToggle = document.getElementById("number-toggle" + id),
+        book_writingToggle = document.getElementById("write-toggle" + id);
+
+    bookList[id].name = nameToggle.value;
+    bookList[id].book_code = book_codeToggle.value;
+    bookList[id].type = typeToggle.value;
+    bookList[id].number = numberToggle.value;
+    bookList[id].book_writing = book_writingToggle.value;
+
+    console.log(nameToggle.value);
+    console.log(book_codeToggle.value);
+    console.log(typeToggle.value);
+    console.log(numberToggle.value);
+    console.log(book_writingToggle.value);
+
+
+    nameToggle.parentNode.innerHTML = "<td id=\"name" + id + "\">" + nameToggle.value + "</td>";
+    book_codeToggle.parentNode.innerHTML = "<td id=\"code" + id + "\">" + book_codeToggle.value + "</td>";
+    typeToggle.parentNode.innerHTML = "<td id=\"type" + id + "\">" + typeToggle.value + "</td>";
+    numberToggle.parentNode.innerHTML = "<td id=\"number" + id + "\">" + numberToggle.value + "</td>";
+    book_writingToggle.parentNode.innerHTML = "<td id=\"write" + id + "\">" + book_writingToggle.value + "</td>";
+
+}
+
+
+// Them object
+
+
+function add(name, book_code, number, book_writing, type) {
+    let nameInput = document.getElementById('name-input').value,
+        codeInput = document.getElementById('code-input').value,
+        numberInput = document.getElementById('number-input').value,
+        writeInput = document.getElementById('write-input').value,
+        typeInput = document.getElementById('type-input').value;
+
+    let book = new Book(nameInput, codeInput, numberInput, writeInput, typeInput);
+    bookList.push(book);
+    showList();
+    nameInput = "";
+    codeInput = "";
+    numberInput = "";
+    writeInput = "";
+    typeInput = "";
+}
+
+
+
 
 
